e5e4bfc0494e2305689ba551f6960bd50d8311f6
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-28 19:58:57 +0530

    Code aaded

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,57 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [styles, setStyles] = useState({
+        color: "rgb(0,0,0)",
+        backgroundColor: "rgb(255,255,255)",
+    });
+    const dark = () => {
+        setStyles({
+            color: "rgb(255,255,255)",
+            backgroundColor: "rgb(0,0,0)",
+        });
+    };
 
-  const [isDarkMode, setIsDarkMode] = useState();
+    const light = () => {
+        setStyles({
+            color: "rgb(0,0,0)",
+            backgroundColor: "rgb(255,255,255)",
+        });
+    };
 
-  return (
-    <div id="main">
-      <h1></h1>
-      <button></button>
-    </div>
-  )
-}
-
+    return (
+        <>
+            <div id="main" style={styles}>
+                {isDarkMode ? (
+                    <>
+                        <h1>Dark Mode Activated</h1>
+                        <button
+                            onClick={() => {
+                                setIsDarkMode(!isDarkMode);
+                                dark();
+                            }}
+                        >
+                            Switch to Light Mode
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <h1>Light Mode Activated</h1>
+                        <button
+                            onClick={() => {
+                                setIsDarkMode(!isDarkMode);
+                                light();
+                            }}
+                        >
+                            Switch to Dark Mode
+                        </button>
+                    </>
+                )}
+            </div>
+        </>
+    );
+};
 
 export default App;
